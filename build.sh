74b99f986a0923ffc7d272ea2a3f31a149dfe15e
glad2os <45284275+glad2os@users.noreply.github.com>
2022-09-26 14:36:44 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker volume create --name=backstop
-docker-compose build
-docker-compose up -d
-
-nginx=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" backstop-nginx)
-nc "${nginx}":22
