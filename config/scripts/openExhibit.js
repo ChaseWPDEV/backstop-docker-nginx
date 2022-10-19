a7b309d92576b61f0daa32b32ef7dd734a17f20f
Chase Gruszewski <chase@cwpdev.com>
2022-10-19 14:15:23 -0400

    multisite read working

--- config/scripts/openExhibit.js
+++ config/scripts/openExhibit.js
@@ -1,7 +1,6 @@
 module.exports=async (page, scenario, viewport, isReference, Engine, config) => {
-    await page.waitForSelector('input[name="startQuiz"]')
-    await page.click('input[name="startQuiz"]');
     await page.waitForSelector('.button.exhibit');
     await new Promise(r => setTimeout(r, 400));
     await page.click('.button.exhibit');
+    await new Promise(r => setTimeout(r, 400));
 }
